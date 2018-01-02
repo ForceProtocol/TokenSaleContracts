18322a16827048512cd639b62424c113bc56b7c5
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2018-01-02 12:04:12 +0530

    Removed ether cap
    Added whitelist logic

--- contracts/triForceNetwork/TriForceNetworkCrowdsale.sol
+++ contracts/triForceNetwork/TriForceNetworkCrowdsale.sol
@@ -2,21 +2,21 @@ pragma solidity ^0.4.11;
 
 import "../crowdsale/singlestage/TokenCappedCrowdsale.sol";
 import "../crowdsale/RefundableCrowdsale.sol";
-import "../crowdsale/singlestage/EthCappedCrowdsale.sol";
 import "../crowdsale/PausableCrowdsale.sol";
+import "../crowdsale/WhiteListedCrowdsale.sol";
 
 
 /**
  * @title TriForceNetworkCrowdsale
  */
-contract TriForceNetworkCrowdsale is TokenCappedCrowdsale, EthCappedCrowdsale, RefundableCrowdsale, PausableCrowdsale {
+contract TriForceNetworkCrowdsale is TokenCappedCrowdsale, RefundableCrowdsale, PausableCrowdsale, WhiteListedCrowdsale {
 
 
-  function TriForceNetworkCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _tokenAddr, address _wallet, uint256 _tokenCap, uint256 _hardCap, uint256 _softCap)
+  function TriForceNetworkCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _tokenAddr, address _wallet, uint256 _tokenCap, uint256 _softCap, address _whitelist)
     Crowdsale(_startTime, _endTime, _rate, _tokenAddr, _wallet)
     TokenCappedCrowdsale(_tokenCap)
     RefundableCrowdsale(_softCap)
-    EthCappedCrowdsale(_hardCap)
+    WhiteListedCrowdsale(_whitelist)
     PausableCrowdsale()
   {
 
@@ -46,7 +46,7 @@ contract TriForceNetworkCrowdsale is TokenCappedCrowdsale, EthCappedCrowdsale, R
   }
 
   // low level token purchase function
-  function buyTokens(address beneficiary) public whenNotPaused payable {
+  function buyTokens(address beneficiary) public whenNotPaused onlyWhiteListed(beneficiary) payable {
     uint256 tokens = _buyTokens(beneficiary, rate.add(rate.mul(bonusFactor()).div(100)));
     if(!setSupply(totalSupply.add(tokens))) revert();
   }
